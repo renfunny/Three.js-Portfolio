91d319576cec4108e0e7e6d99201aeb694c174c6
Renato Cordova <ren99cordova@gmail.com>
2023-08-14 15:30:22 -0400

    Contact email functionality has been finalized, the skeleton of the projects is done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,47 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_dwwb4yh",
+        "template_5471cjj",
+        {
+          from_name: form.name,
+          to_name: "Renato",
+          from_email: form.email,
+          to_email: "ren99cordova@gmail.com",
+          message: form.message,
+        },
+        "xO6023TxciPqDRE84"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
